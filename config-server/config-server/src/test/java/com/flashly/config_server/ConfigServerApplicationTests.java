1b5ad15f6e76d492c7df866215f173e392604820
NirakChoun <nirakchoun@gmail.com>
2025-11-09 12:07:50 -0600

    Initial commit

---
+++ config-server/config-server/src/test/java/com/flashly/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashly.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
